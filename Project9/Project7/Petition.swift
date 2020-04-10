385afc0ce0b6e1f7fe925d7a3721df75bc90f630
paigupai <44311361+paigupai@users.noreply.github.com>
2020-04-10 20:20:59 +0900

    project9

---
+++ Project9/Project7/Petition.swift
@@ -0,0 +1,15 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by paigu on 2020/04/08.
+//  Copyright © 2020 paigu. All rights reserved.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project9/Project7/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by paigu on 2020/04/08.
+//  Copyright © 2020 paigu. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
